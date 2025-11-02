d4e570d625386ec801be0ab2752703d99b614498
Lucas Alves <sociallucasalves@gmail.com>
2025-11-02 16:02:27 -0300

    refeito, agora com menu inicial

--- main.asm
+++ main.asm
@@ -1,387 +1,302 @@
 ; M0 ? Sanity check: texto -> 13h -> desenha 1 pixel -> volta pro texto
 .model small
-.stack 100h
+.stack
 
 .data
+    seed dw 0
+    menu db 0
+    btn_iniciar db  14 dup(" "),218,196,196,196,196,196,196,196,196,196,191,13,10
+                 db 14 dup(" "),179,"  JOGAR  ",179,10,13
+                 db 14 dup(" "),192,196,196,196,196,196,196,196,196,196,217,13,10
+
+    btn_iniciar_length equ $-btn_iniciar
+
+    btn_sair db  14 dup(" "),218,196,196,196,196,196,196,196,196,196,191,13,10
+              db 14 dup(" "),179,"  SAIR   ",179,10,13
+              db 14 dup(" "),192,196,196,196,196,196,196,196,196,196,217,13,10
+
+    btn_sair_length equ $-btn_sair
     CR EQU 13
     LF EQU 10
     SPR_W  EQU 13
     SPR_H  EQU 29
-.data
-    CR EQU 13
-    LF EQU 10
-    SPR_W  EQU 13
-    SPR_H  EQU 29
-    sprite_ally db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,1,0,0,0,0,1,0,0
-                db 0,0,0,0,1,5,1,0,1,5,1,0,0
-                db 0,0,0,0,0,1,0,0,0,1,0,0,0
-                db 0,0,0,1,1,4,1,1,1,4,1,1,0
-                db 0,0,0,1,4,4,4,4,4,4,4,1,0
-                db 0,0,1,1,4,4,4,4,4,4,4,1,1
-                db 0,0,1,4,4,4,4,4,4,4,4,4,1
-                db 0,1,1,4,4,1,4,1,4,4,4,4,1
-                db 0,1,4,4,4,4,4,4,4,4,4,4,1
-                db 1,5,4,4,4,4,4,4,4,4,4,5,1
-                db 0,1,4,4,4,4,4,4,4,4,4,4,1
-                db 0,1,1,4,4,4,4,4,4,4,4,1,1
-                db 0,0,1,4,4,4,4,4,4,4,4,1,0
-                db 0,0,0,1,1,4,1,1,1,4,1,1,0
-                db 0,0,0,0,0,1,0,0,0,1,0,0,0
-                db 0,0,0,0,1,5,1,0,1,5,1,0,0
-                db 0,0,0,0,0,1,0,0,0,1,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
-                db 0,0,0,0,0,0,0,0,0,0,0,0,0
+    string  db 7 dup(" ")," ___                    ",13,10
+            db 7 dup(" "),"/ __| __ _ _ __ _ _ __  ",13,10
+            db 7 dup(" "),"\__ \/ _| '_/ _` | '  \ ",13,10
+            db 7 dup(" "),"|___/\__|_|_\__,_|_|_|_|",13,10
+            db 7 dup(" "),"     | |__| |___        ",13,10
+            db 7 dup(" "),"     | '_ \ / -_)       ",13,10
+            db 7 dup(" "),"     |_.__/_\___|       ",13,10
+
+            
+            
+    string_length equ $-string
+    meteor_sprite   db 0,0,0,0,0,0,0,1,1,0,0,0,0,0
+                    db 0,0,0,0,1,1,4,4,4,1,0,0,0,0
+                    db 0,0,0,1,4,4,4,4,4,4,1,0,1,1
+                    db 0,0,1,4,4,4,5,5,4,4,4,1,0,0
+                    db 0,1,4,4,5,5,5,5,5,4,4,1,0,0
+                    db 0,1,4,4,5,5,5,5,5,5,4,1,5,5
+                    db 0,1,4,4,4,5,5,5,5,4,4,1,4,5
+                    db 0,0,1,4,4,4,4,5,4,4,1,0,0,0
+                    db 0,0,1,4,4,4,4,4,4,4,1,0,1,5
+                    db 0,0,0,1,4,4,4,4,4,1,0,0,0,0
+                    db 0,0,0,0,1,4,4,4,1,0,4,4,0,0
+                    db 0,0,0,0,0,1,1,1,0,0,1,0,0,0
+                    db 0,0,0,0,0,0,0,0,0,0,0,0,0,0
+                    db 0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    meteor_pos dw 100*320 + 300 ; posi??o inicial (linha 100, x = 300)
+
     
 .code
-INICIO:
-    mov  ax, @data
-    mov  ds, ax
+MAIN:
+    mov AX, @data
+    mov DS, AX
+    mov AX, 0A000H
+    mov ES, AX
+    xor DI, DI
 
-    ; Entra no v?deo 13h e aponta ES pra A000h
-    call VIDEO13_INIT
-    mov al, 1Fh
-    call CLEAR13
+    ; call SYSTIME_SEED
 
-    ; Linha no topo (y=0), largura 320, cor 0Fh
-    mov bx, 0      ; x0
-    mov cx, 0      ; y
-    mov dx, 640    ; largura
-    mov al, 0Fh
-    call HLINE
+    ; Define o modo de video
+    xor ah, ah
+    xor bh, bh
+    mov al, 13h
+    int 10h
 
-    ; Linha embaixo (y=199), largura 320, cor 1Eh
-    mov bx, 0
-    mov cx, 199
-    mov dx, 640
-    mov al, 1Eh
-    call HLINE
-
-    mov  bx, 0
-    mov  cx, 0
-    mov  dx, 200   ; altura
-    call VLINE
-
-    mov  bx, 319
-    mov  cx, 0
-    mov  dx, 200
-    call VLINE
+    ; Exibe titulo e botoes do menu
+    call PRINT_TITLE_MENU
+    call PRINT_BUTTONS
     
-    
-    ; ret?ngulo central 
-    mov  bx, 135   ; x
-    mov  cx, 160    ;y
-    mov  si, 50   ; largura
-    mov  di, 20    ; altura
-    mov  al, 10h
-    call RECTFILL
-    
+LOOP_MENU:
+    call MOVE_METEOR
+    jmp LOOP_MENU
 
-    ; ret?ngulo central
-    mov  bx, 135   ; x
-    mov  cx, 135    ;y
-    mov  si, 50   ; largura
-    mov  di, 20    ; altura
-    mov  al, 10h
-    call RECTFILL
-    
-   ; desenha opaco em (40,40)
-    mov  bx, 40
-    mov  cx, 40
-    mov  si, offset sprite_ally
-    call BLIT_SPRITE_OPAQUE
-
-    ; desenha transparente em (120,60)
-    mov  bx, 120
-    mov  cx, 60
-    mov  si, offset sprite_ally
-    call BLIT_SPRITE_TRANS
-    
-    ; espera ~5s
-    mov  ah, 86h
-    mov  cx, 004Ch
-    mov  dx, 4B40h
-    int  15h
-
-    
-    ; espera ~5s
-    mov  ah, 86h
-    mov  cx, 004Ch
-    mov  dx, 4B40h
-    int  15h
-    call VIDEO_TEXT
-
-    
-    mov  ax, 4C00h
-    int  21h
-
-;-----------------------------------------
-; Procs utilit?rias (estilo do professor)
-;-----------------------------------------
-
-; Quebra de linha (CR/LF) - usa AH=02h
-NL PROC
+; Procedimento para exibir os botoes INICIAR e SAIR
+; se menu == 0 o bot?o jogar fica vermelho
+; se menu == 1 o botao sair fica vermelho
+PRINT_BUTTONS proc
     push ax
-    push dx
-    mov  dl, CR
-    mov  ah, 02h
-    int  21h
-    mov  dl, LF
-    mov  ah, 02h
-    int  21h
-    pop  dx
-    pop  ax
+    mov bl, 0FH
+    mov ah, menu
+    cmp ah, 0
+    jne START_BTN
+    mov bl, 0CH
+
+START_BTN:
+    ; Exibe o bot??o INICIAR
+    mov bp, offset btn_iniciar
+    mov cx, btn_iniciar_length ; tamanho
+    xor dl, dl ; coluna = 0
+    mov dh, 18 ; linha = 18
+    call PRINT_STRING
+
+    mov bl, 0FH
+    mov ah, menu
+    cmp ah, 1
+    jne EXIT_BTN
+    mov bl, 0CH
+
+EXIT_BTN:
+    mov bp, offset btn_sair
+    mov cx, btn_sair_length ; tamanho
+    xor dl, dl ; coluna = 0
+    mov dh, 21 ; linha = 21
+    call PRINT_STRING
+
+    pop ax
     ret
-NL ENDP
+PRINT_BUTTONS endp
 
-; Inicializa modo 13h e ES=A000h, CLD para strings
-VIDEO13_INIT PROC
-    push ax
-    mov  ax, 0013h
-    int  10h
-    mov  ax, 0A000h
-    mov  es, ax
-    cld
-    pop  ax
-    ret
-VIDEO13_INIT ENDP
 
-; Volta ao modo texto 80x25
-VIDEO_TEXT PROC
-    push ax
-    mov  ax, 0003h
-    int  10h
-    pop  ax
-    ret
-VIDEO_TEXT ENDP
-
-; PUTPIXEL ? Entrada: BX=x (0..319), CX=y (0..199), AL=cor
-; Usa ES:A000h. Calcula offset: y*320 + x = y*256 + y*64 + x
-PUTPIXEL PROC
-    push dx
-    push di
-
-    mov  di, cx      ; di = y
-    shl  di, 6       ; y*64
-    mov  dx, cx
-    shl  dx, 8       ; y*256
-    add  di, dx      ; y*256 + y*64
-    add  di, bx      ; + x
-    mov  es:[di], al
-
-    pop  di
-    pop  dx
-    ret
-PUTPIXEL ENDP
-
-; CLEAR13 ? preenche a tela 320x200 com a cor AL
-; Pr?: ES = A000h (chame VIDEO13_INIT antes), CLD setado
-CLEAR13 PROC
-    push cx
-    push di
-    xor  di, di
-    mov  cx, 320*200      ; 64.000 pixels
-    rep  stosb            ; escreve AL em ES:[DI], CX vezes
-    pop  di
-    pop  cx
-    ret
-CLEAR13 ENDP
-
-; HLINE ? desenha linha horizontal
-; Entradas: BX=x0, CX=y, DX=largura, AL=cor
-; Pr?: ES=A000h, CLD
-HLINE PROC
-    push ax
-    push dx
-    push di
-
-    ; DI = y*320 + x
-    mov  di, cx
-    shl  di, 6
-    mov  ax, cx
-    shl  ax, 8
-    add  di, ax
-    add  di, bx
-
-    mov  cx, dx      ; CX = largura
-    rep  stosb       ; escreve AL em ES:[DI], CX vezes
-
-    pop  di
-    pop  dx
-    pop  ax
-    ret
-HLINE ENDP
-
-VLINE PROC
+SYSTIME_SEED proc
     push ax
     push cx
     push dx
-    push di
-    ; DI = y*320 + x
-    mov  di, cx
-    shl  di, 6
-    mov  ax, cx
-    shl  ax, 8
-    add  di, ax
-    add  di, bx
 
-    mov  cx, dx      ; CX = altura
-    VL_S_LOOP:
-        stosb              ; escreve AL em ES:[DI] e DI += 1
-        add   di, 319      ; total do passo = 1 + 319 = 320
-        loop  VL_S_LOOP
+    xor ax, ax
+    int 1AH
+    mov seed, dx
 
-        pop  di
-        pop  dx
-        pop  cx
-        pop  ax
-        ret
+    pop dx
+    pop cx
+    pop ax
+    ret
+SYSTIME_SEED endp
+
+;  Desenha CX caracteres a partir de ES:BP na posi??o (linha DH, coluna DL).
+;  Usa a cor/atributo em BL (p.ex.: 0Fh = branco, 0Ch = vermelho-claro).
+;  Atualiza o cursor ap?s imprimir (modo AL=1).
+;  ES deve apontar para o segmento onde est? a string (comumente ES=DS).
+;  BP deve conter o offset da string dentro de ES.
+PRINT_STRING PROC
+    push AX
+    push BX
+    push DS
+    push ES
+    push SI
+    push BP
+
+    mov ah, 13h         
+    mov al, 1          
+                        
+    xor bh, bh           
+    int 10h  ; escreve string          
+
+    pop BP
+    pop SI
+    pop ES
+    pop DS
+    pop BX
+    pop AX
+    ret
+PRINT_STRING ENDP
+
+PRINT_TITLE_MENU proc
+    mov ax, ds 
+    mov es, ax
     
-        ;rep  stosb       ; escreve AL em ES:[DI], CX vezes
+    ; ajusta resgitradores pro PRINT_STRING
+    mov bp, offset string
+    mov cx, string_length ; tamanho
+    mov bl, 02H ; Cor verde (se bit 1 de AL estiver limpo, usamos BL)
+    xor dx, dx ; linha / coluna
+    call PRINT_STRING
 
-; RECTFILL ? preenche ret?ngulo LxA
-; BX=x, CX=y, SI=largura, DI=altura, AL=cor | Pr?: ES=A000h, CLD
-RECTFILL PROC
-    push ax
+    ret
+PRINT_TITLE_MENU endp
+
+RENDER_SPRITE proc
+    ; joga pra pilha valres dos registradores pra n?o perder
     push bx
     push cx
     push dx
-    push si
     push di
-    push bp
-
-    mov  bp, di         ; bp = altura (guardamos porque DI vira offset)
-
-    ; DI = y*320 + x
-    mov  di, cx
-    shl  di, 6
-    mov  dx, cx
-    shl  dx, 8
-    add  di, dx
-    add  di, bx
-
-    RF_Y_LOOP:
-        mov  cx, si         ; cx = largura
-        rep  stosb          ; pinta uma linha com AL
-
-        ; avan?a para a pr?xima linha: +320 - largura
-        mov  dx, 320
-        sub  dx, si
-        add  di, dx
-
-        dec  bp
-        jnz  RF_Y_LOOP
-
-        pop  bp
-        pop  di
-        pop  si
-        pop  dx
-        pop  cx
-        pop  bx
-        pop  ax
-        ret
-RECTFILL ENDP
-
-; BLIT_SPRITE_OPAQUE ? copia SPR_W bytes por linha, SPR_H linhas
-; BX=x, CX=y, SI=src (DS), Pr?: ES=A000h, CLD
-BLIT_SPRITE_OPAQUE PROC
+    push es
+    push ds
     push ax
-    push bx
+
+    ; ds apotando pros dados onde a sprite t?
+    mov ax, @data
+    mov ds, ax
+
+    ; garante que continua no modo 13h
+    mov ax, 0A000h
+    mov es, ax
+
+    ; pega da pilha o ax (posi??o da tela)
+    ; joga pra di do movsb
+    pop ax
+    mov di, ax
+    mov dx, 14 ; 14 linhas pro loop
+    push ax
+
+DRAW_LINE:
+    mov cx, 14 ; 14 colunas
+    rep movsb
+    add di, 320 - 14 ; avan?a pro come?o da prox linha
+    dec dx
+    jnz DRAW_LINE
+
+    ; devolve os valores empilhados no comeco
+    pop ax
+    pop ds  
+    pop es
+    pop di
+    pop dx
+    pop cx
+    pop bx
+ret
+RENDER_SPRITE endp
+
+
+MOVE_METEOR proc
+    ; joga pra pilha valres dos registradores pra n?o perder
+    push ax
+    push di
+    push si
     push cx
     push dx
-    push si
-    push di
-    push bp
 
-    mov  bp, SPR_H       ; contador de linhas
+    ; Apaga sprite atual
+    ; joga em ax a posi??o do meteoro
+    ; copia pra di isso pra ser usado na proc
+    mov ax, meteor_pos
+    mov di, ax
+    call CLEAR_SPRITE
 
-    ; DI0 = y*320 + x
-    mov  di, cx
-    shl  di, 6
-    mov  dx, cx
-    shl  dx, 8
-    add  di, dx
-    add  di, bx
+    ; Verifica se chegou no limite esquerdo (coluna 0)
+    ; joga de novo a posi??o do meteoro pra ax
+    ; verifica se ax ja chegou no limite esquerdo da tela
+    mov ax, meteor_pos
+    cmp ax, 100*320
+    ja CONTINUE_MOVE
 
-    ROW_LOOP_OP:
-        mov  cx, SPR_W       ; bytes nesta linha
-        rep  movsb           ; copia DS:[SI..] -> ES:[DI..]
-        ; ajustar DI para a pr?xima linha: +320 - SPR_W
-        mov  ax, 320
-        sub  ax, SPR_W
-        add  di, ax
-        dec  bp
-        jnz  ROW_LOOP_OP
+    ; Se passou do limite, reinicia no lado direito
+    ; coloca a posi??o do meteoro no canto direito
+    mov ax, 100*320 + 306
+    mov meteor_pos, ax
+    jmp DRAW_METEOR
 
-        pop  bp
-        pop  di
-        pop  si
-        pop  dx
-        pop  cx
-        pop  bx
-        pop  ax
-        ret
-BLIT_SPRITE_OPAQUE ENDP
+CONTINUE_MOVE:
+    ; Move o meteoro pra esquerda
+    ; o movimento ? feito diminindo um pixel da posi??o
+    dec meteor_pos
+    dec ax
 
-; BLIT_SPRITE_TRANS ? 0 n?o desenha; ?0 desenha
-; BX=x, CX=y, SI=src (DS), Pr?: ES=A000h, CLD
-BLIT_SPRITE_TRANS PROC
+DRAW_METEOR:
+    ; coloca o sprint em si
+    mov si, offset meteor_sprite
+    call RENDER_SPRITE
+
+    ; Delay
+    ; usa interrup??o 15h que faz o delay
+    ; dx fica com o valor do deplay, quanto maior mais delay tera
+    xor cx, cx
+    mov dx, 4000h
+    mov ah, 86h
+    int 15h
+
+    ; devolve os valores empilhados no comeco
+    pop dx
+    pop cx
+    pop si
+    pop di
+    pop ax
+    ret
+MOVE_METEOR endp
+
+CLEAR_SPRITE proc
+    ; joga pra pilha valres dos registradores pra n?o perder
     push ax
-    push bx
     push cx
-    push dx
-    push si
     push di
-    push bp
+    push es
+    
+    ; aponta pra 0A000H no es pra fazer os desenhos na tela
+    ; define cx = 14 pra informar o numero de linha
+    mov ax, 0A000H
+    mov es, ax
+    mov cx, 14
 
-    mov  bp, SPR_H ; contador de linhas do desenho
+CLEAR_LINE:
+    ; define cx = 14 pra informar o numero de colunas
+    ; usa xor pra zerar ax (0 = preto)
+    ; usa stobs pra colocar preto em todos os cx pixeis 
+    push cx
+    mov cx, 14
+    xor ax, ax
+    rep stosb
+    add di, 306
+    pop cx
+    loop CLEAR_LINE
 
-    ; DI0 = y*320 + x
-    mov  di, cx
-    shl  di, 6
-    mov  dx, cx
-    shl  dx, 8
-    add  di, dx
-    add  di, bx
+    ; devolve os valores empilhados no come?o
+    pop es
+    pop di
+    pop cx
+    pop ax
+    ret
+CLEAR_SPRITE endp
 
-    ROW_LOOP_TR:
-        mov  cx, SPR_W
-    COL_LOOP_TR:
-        lodsb                ; AL = [DS:SI], SI++
-        or   al, al          ; AL == 0 ?
-        jz   skip_px
-        stosb                ; escreve AL em ES:[DI], DI++
-        jmp  short next_px
-    skip_px:
-        inc  di              ; s? avan?a destino
-    next_px:
-        loop COL_LOOP_TR
-
-        ; pr?xima linha
-        mov  ax, 320
-        sub  ax, SPR_W
-        add  di, ax
-        dec  bp
-        jnz  ROW_LOOP_TR
-
-        pop  bp
-        pop  di
-        pop  si
-        pop  dx
-        pop  cx
-        pop  bx
-        pop  ax
-        ret
-BLIT_SPRITE_TRANS ENDP
-
-
-end INICIO
+end MAIN
